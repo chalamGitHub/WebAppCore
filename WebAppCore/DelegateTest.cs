9c402a598aaf92ff89811710cc74cb542c0181ff
Simhachalam Naidu <Simhachalam Naidu@SIMHACHALAM>
2022-11-30 11:16:51 -0600

    Add project files.

---
+++ WebAppCore/DelegateTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCore
+{
+    public class DelegateTest
+    {
+        private delegate int FuncTwoInts(int one, int two);
+
+        private static int Add(int one, int two)
+        {
+            return one + two;
+        }
+
+        private FuncTwoInts theFunc = Add;
+
+
+        FuncTwoInts theFunc2 = delegate (int one, int two)
+        {
+            return one + two;
+        };
+
+
+        FuncTwoInts theFunc1 = (one, two) =>
+        {
+            return one + two;
+        };
+    }
+}
